ac89d26e9105f1cf27004d3d287fb62ba69ddd67
Stefan Mitrik <stevo.mit@gmail.com>
2010-10-24 18:41:31 +0200

    Autoradio pocitanie frekvencii surove posuvanie

--- Autoradio/Views/Radio.xaml.cs
+++ Autoradio/Views/Radio.xaml.cs
@@ -16,11 +16,26 @@ namespace Autoradio.Views
 {
     public partial class Radio : Page, IModuleInterface
     {
+        private const int minPozicia = -413;
+        private const int maxPozicia = 455;
+        private const double minimalnaFrekvencia = 87;
+        private const double maximalnaFrekvencia = 108;
+        private const double pocetFrekvencii = maximalnaFrekvencia - minimalnaFrekvencia;
+        private const double dlzkaFrekvencie = (maxPozicia + (-1 * minPozicia)) / pocetFrekvencii;
+
+        private double aktualnaFrekvencia;
+        private int poziciaFrekvencie;
+
         private StateChangedNotify stateChanged;
+        
 
         public Radio()
         {
             InitializeComponent();
+
+            poziciaFrekvencie = 0;
+            prepocitajFrekvenciu();
+            
         }
 
         // Executes when the user navigates to this page.
@@ -31,6 +46,41 @@ namespace Autoradio.Views
         public void initialize(StateChangedNotify stateChanged, Playlist playlist)
         {
             this.stateChanged = stateChanged;
+        }
+
+
+        private void prepocitajFrekvenciu()
+        {
+            aktualnaFrekvencia = (double)((poziciaFrekvencie + -1 * minPozicia) / dlzkaFrekvencie);
+            aktualnaFrekvencia = maximalnaFrekvencia - aktualnaFrekvencia;
+            frekvencia_text.Text = aktualnaFrekvencia.ToString();
+        }
+
+
+        private void button_left_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            poziciaFrekvencie -= 50;
+
+            if (poziciaFrekvencie < minPozicia)
+                poziciaFrekvencie = minPozicia;
+
+            MyDoubleAnimation.To = poziciaFrekvencie;
+            Posuv_frekvencie.Begin();
+            prepocitajFrekvenciu();
+               
+		}
+
+        private void button_right_Click(object sender, RoutedEventArgs e)
+        {
+            poziciaFrekvencie += 50;
+
+            if (poziciaFrekvencie > maxPozicia)
+                poziciaFrekvencie = maxPozicia;
+
+            MyDoubleAnimation.To = poziciaFrekvencie;
+            Posuv_frekvencie.Begin();
+            prepocitajFrekvenciu();
+            
         }
 
     }
